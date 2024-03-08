9d77f4fde649dc0a6a58fdd13dafb8a240b085dd
Lázaro Pimentel <118478127+LazaroAmvox@users.noreply.github.com>
2024-03-08 12:21:26 -0300

    commit: Alterar status favorito e visualizar lista de favoritos

--- agenda.py
+++ agenda.py
@@ -16,8 +16,6 @@ def visualizar_contato(contatos):
 
 
 def editar_contato(contatos, indice_contato, nome_contato_atualizado, email_contato_atualizado, telefone_contato_atualizado):
-    print("\n Editar Contato")
-    print(telefone_contato_atualizado, email_contato_atualizado)
     indice_contato_ajustado = int(indice_contato) - 1
     if indice_contato_ajustado >= 0 and indice_contato_ajustado < len (contatos):
         if len(nome_contato_atualizado) > 0:
@@ -31,15 +29,32 @@ def editar_contato(contatos, indice_contato, nome_contato_atualizado, email_cont
         print("Índice de contato inválido.")
     return
 
+def alterar_status_favorito(contatos, indice_contato):
+    indice_contato_ajustado = int(indice_contato) - 1
+    status = contatos[indice_contato_ajustado]["Favorito"]
+
+    if(status):
+        contatos[indice_contato_ajustado]["Favorito"] = False
+        print(f"Contato removido dos Favoritos")
+    else:
+        contatos[indice_contato_ajustado]["Favorito"] = True
+        print(f"Contato adicionado aos Favoritos")
+
+def visualizar_favorito(contatos):
+    for indice, contato in enumerate(contatos):
+        if(contatos[indice]["Favorito"]):
+            print(f"[💖] - Nome: {contatos[indice]["Nome"]}, Telefone: {contatos[indice]["Telefone"]}, Email: {contatos[indice]["Email"]}")
+
 contatos = []
 while True:
     print("\nGerenciador de Lista de contatos:")
     print("1. Adicionar Contatos")
     print("2. Ver Contatos")
     print("3. Atualizar Contatos")
-    print("4. Favoritar Contatos")
-    print("5. Deletar Contatos")
-    print("6. Sair")
+    print("4. Favoritar/Desfavoritar Contatos")
+    print("5. Ver Contatos Favoritos")
+    print("6. Deletar Contatos")
+    print("7. Sair")
 
     escolha = input("\nDigite a sua escolha:")
 
@@ -53,10 +68,15 @@ while True:
     elif escolha == "3":
         visualizar_contato(contatos)
         indice_contato = input("O digite o numero da Lista de contato que deseja atualizar: ")
-        nome_contato_atualizado = input("Digite o nome do contato atual do contato ou dar Enter: ")
+        nome_contato_atualizado = input("Digite o nome do contato atual ou dar Enter: ")
         telefone_contato_atualizado = input("Digite o telefone atual do contato ou dar Enter: ")
         email_contato_atualizado = input("Digite o email atual do contato ou dar Enter: ")
         editar_contato(contatos, indice_contato, nome_contato_atualizado, email_contato_atualizado, telefone_contato_atualizado)
         visualizar_contato(contatos)
+    elif escolha == "4":
+        indice_contato = input("O digite o numero da Lista de contato que deseja alterar o status de favorito: ")
+        alterar_status_favorito(contatos, indice_contato)
+    elif escolha == "5":
+        visualizar_favorito(contatos)
     else:
        print("Encerrado")
